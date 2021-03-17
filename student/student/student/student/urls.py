f928fe700fa32097a4f17daf7e9803b6cd02fa8e
shreyas khele <80670611+shreyakhele@users.noreply.github.com>
2021-03-17 14:39:19 +0530

    Add files via upload

---
+++ student/student/student/student/urls.py
@@ -0,0 +1,28 @@
+"""student URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from studentapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.display_data, name='display'),
+    path('add_student',views.studentdata_view, name='add_student'),
+    path('update_data/<id>',views.updatedata_view, name='update_data'),
+    path('save/<id>',views.save_view, name='save'),
+    path('delete/<id>',views.delete_data, name='delete_data')
+
+
+]
